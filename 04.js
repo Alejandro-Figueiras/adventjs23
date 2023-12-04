9692d82f2b3cae4dbf3695c8124dea0e787be2f1
Alejandro <alejandrofigueirasx@gmail.com>
2023-12-04 15:18:52 -0500

    Reto 04 Optimizado

--- 04.js
+++ 04.js
@@ -1,24 +1,25 @@
 function decode(message) {
   const msg = message.split("");
   const swap = (a,b) => {
-    const tmp = msg[a];
+    const tmp = msg[a]; 
     msg[a] = msg[b];
     msg[b] = tmp;
-    console.log(msg)
   }
   // Code here
   let stack = [];
   for (let i = 0; i < msg.length; i++) {
-    if (msg[i] == '(') stack.push(i);
+    if (msg[i] == '(') {
+      stack.push(i);
+      msg[i] = ''
+    }
     if (msg[i] == ')') {
+      msg[i] = ''
       const pos = stack.pop();
       for (let j = 0; j <= (i-pos)/2-1; j++) {
-        console.log("encontrado")
-        
         swap(pos+j+1, i-1-j)
       }
     }
   }
   
-  return msg.join("").split("(").join('').split(')').join("");
+  return msg.join("")
 }
